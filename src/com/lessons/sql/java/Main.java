1df9f48018e76e3267b8d8d31649055d70484b67
Denis-92 <d.mozenic@gmail.com>
2023-02-08 16:24:33 +0100

    fatto: MILESTONE 3

--- src/com/lessons/sql/java/Main.java
+++ src/com/lessons/sql/java/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,20 +15,25 @@ public class Main {
 		String user = "root";
 		String password = "root";
 		
-		//Connection con = null;
-		//con = DriverManager.getConnection(url, user, password);
+		Scanner inputUtente = new Scanner(System.in);
 		
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 			
-			String sql = 
-					"SELECT countries.name AS Nome_stato, countries.country_id AS ID_stato, regions.name AS Nome_regione, continents.name AS Nome_continente\r\n"
-					+ "FROM countries\r\n"
-					+ "INNER JOIN regions ON countries.region_id = regions.region_id\r\n"
-					+ "INNER JOIN continents ON regions.continent_id = continents.continent_id\r\n"
-					+ "GROUP BY ID_stato, Nome_regione, Nome_continente\r\n"
-					+ "ORDER BY Nome_stato;";
+			System.out.print("Inserisci filtro di ricerca: ");
+			String inputFiltro = inputUtente.nextLine();
+			String filtroRicerca = "%" + inputFiltro + "%";
+			
+			 String sql = 
+						"SELECT countries.name AS Nome_stato, countries.country_id AS ID_stato, regions.name AS Nome_regione, continents.name AS Nome_continente\r\n"
+						+ "FROM countries\r\n"
+						+ "INNER JOIN regions ON countries.region_id = regions.region_id\r\n"
+						+ "INNER JOIN continents ON regions.continent_id = continents.continent_id\r\n"
+						+ "WHERE countries.name LIKE ? \r\n"
+						+ "GROUP BY ID_stato, Nome_regione, Nome_continente\r\n"
+						+ "ORDER BY Nome_stato;";
 			
 			try(PreparedStatement ps = con.prepareStatement(sql)) {
+				ps.setString(1, filtroRicerca);
 			
 				try(ResultSet rs = ps.executeQuery()) {
 					System.out.println("NOME STATO \t\t ID STATO \t\t NOME REGIONE \t\t NOME CONTINENTE");
@@ -46,6 +52,19 @@ public class Main {
 			exception.printStackTrace();
 		}
 		
+		inputUtente.close();
+		
 	}
 
 }
+
+/*
+MILESTONE 1 e 2
+ String sql = 
+					"SELECT countries.name AS Nome_stato, countries.country_id AS ID_stato, regions.name AS Nome_regione, continents.name AS Nome_continente\r\n"
+					+ "FROM countries\r\n"
+					+ "INNER JOIN regions ON countries.region_id = regions.region_id\r\n"
+					+ "INNER JOIN continents ON regions.continent_id = continents.continent_id\r\n"
+					+ "GROUP BY ID_stato, Nome_regione, Nome_continente\r\n"
+					+ "ORDER BY Nome_stato;";
+*/
